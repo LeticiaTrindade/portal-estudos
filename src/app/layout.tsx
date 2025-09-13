d9165704c3d74041aba17b1c675fe71f6dd1d9f1
Letícia Trindade <leticiatrindade0504@gmail.com>
2025-09-13 13:01:02 -0300

    criando o primeiro exercicio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import "./globals.css"; // Certifique-se de que o CSS global está importado aqui
 
 type RootLayoutProps = {
